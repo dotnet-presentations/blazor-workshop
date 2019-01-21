0bfaf748a0f2c6025eb3823f92b0dd72c5e33fe7
Ryan Nowak <nowakra@gmail.com>
2019-01-20 22:35:26 -0800

    Rough cut of the customize form

--- src/BlazingPizza.Shared/PizzaSpecial.cs
+++ src/BlazingPizza.Shared/PizzaSpecial.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace BlazingPizza
 {
@@ -11,8 +12,30 @@ namespace BlazingPizza
 
         public string Name { get; set; }
 
+        public decimal BasePrice { get; set; }
+
         public string Description { get; set; }
 
+        public string ImageUrl { get; set; }
+
         public List<PizzaSpecialTopping> Toppings { get; set; }
+
+        public decimal GetPriceForSize(PizzaSize size)
+        {
+            switch (size)
+            {
+                case PizzaSize.Small:
+                    return BasePrice * .75m;
+
+                case PizzaSize.Medium:
+                    return BasePrice * 1.00m;
+
+                case PizzaSize.Large:
+                    return BasePrice * 1.25m;
+
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
     }
 }
