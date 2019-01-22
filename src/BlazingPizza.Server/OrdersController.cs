058181fc9ff6bd6e184ba28fcae990118c4ae081
Ryan Nowak <nowakra@gmail.com>
2019-01-21 19:28:35 -0800

    Add actual order processing

---
+++ src/BlazingPizza.Server/OrdersController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Server
+{
+    [Route("orders")]
+    [ApiController]
+    public class OrdersController : Controller
+    {
+        private readonly PizzaStoreContext _db;
+
+        public OrdersController(PizzaStoreContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Order>>> GetOrders()
+        {
+            return await _db.Orders.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PlaceOrder(Order order)
+        {
+            _db.Orders.Attach(order);
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
