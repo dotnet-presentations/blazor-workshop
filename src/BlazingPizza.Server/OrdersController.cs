c7c81fe5e9d110b495a20b7bad106421fe23b3df
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-01-23 00:01:58 +0000

    Enforce authorization. Trigger login before order submission.

--- src/BlazingPizza.Server/OrdersController.cs
+++ src/BlazingPizza.Server/OrdersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,7 @@ namespace BlazingPizza.Server
 {
     [Route("orders")]
     [ApiController]
+    [Authorize]
     public class OrdersController : Controller
     {
         private readonly PizzaStoreContext _db;
