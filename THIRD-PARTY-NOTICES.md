eb2962b92994a66518564b80de6c9fb15412793c
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-01-27 10:05:08 +0000

    Add pizza images

--- THIRD-PARTY-NOTICES.md
+++ THIRD-PARTY-NOTICES.md
@@ -9,5 +9,19 @@ This repo contains materials from third parties, supplied under the following li
   Copyright 2011 The Quicksand Project Authors (https://github.com/andrew-paglinawan/QuicksandFamily),
   with Reserved Font Name. Licensed under SIL (http://scripts.sil.org/OFL)
 
-* pizza-1.jpg
-  Photo by Thomas Tucker on Unsplash (https://unsplash.com/license)
+* meaty.jpg
+  Photo by Matteo Vistocco on Unsplash (https://unsplash.com/license)
+* cheese.jpg
+  Photo by Evelyn on Unsplash (https://unsplash.com/license)
+* pepperoni.jpg
+  Photo by Alan Hardman on Unsplash (https://unsplash.com/license)
+* margherita.jpg
+  Photo by rawpixel on Unsplash (https://unsplash.com/license)
+* salad.jpg
+  Photo by Jose Soriano on Unsplash (https://unsplash.com/license)
+* mushroom.jpg
+  Photo by Lavi Perchik on Unsplash (https://unsplash.com/license)
+* brit.jpg
+  Photo by Chris Lawton on Unsplash (https://unsplash.com/license)
+* bacon.jpg
+  Photo by Roberto Valdivia on Unsplash (https://unsplash.com/license)
--- save-points/03-show-order-status/BlazingPizza.Server/SeedData.cs
+++ save-points/03-show-order-status/BlazingPizza.Server/SeedData.cs
@@ -125,9 +125,9 @@ namespace BlazingPizza.Server
                 new PizzaSpecial()
                 {
                     Name = "Basic Cheese Pizza",
-                    Description = "It's cheesy and delicious, why wouldn't you want one?",
+                    Description = "It's cheesy and delicious. Why wouldn't you want one?",
                     BasePrice = 9.99m,
-                    ImageUrl = "/img/pizzas/pizza-1.jpg",
+                    ImageUrl = "img/pizzas/cheese.jpg",
                 },
                 new PizzaSpecial()
                 {
@@ -135,15 +135,15 @@ namespace BlazingPizza.Server
                     Name = "The Baconatorizor",
                     Description = "It has EVERY kind of bacon",
                     BasePrice = 11.99m,
-                    ImageUrl = "/img/pizzas/pizza-1.jpg",
+                    ImageUrl = "img/pizzas/bacon.jpg",
                 },
                 new PizzaSpecial()
                 {
                     Id = 3,
                     Name = "Classic pepperoni",
-                    Description = "It's the pizza you grew up, but Blazing hot!",
+                    Description = "It's the pizza you grew up with, but Blazing hot!",
                     BasePrice = 10.50m,
-                    ImageUrl = "/img/pizzas/pizza-1.jpg",
+                    ImageUrl = "img/pizzas/pepperoni.jpg",
                 },
                 new PizzaSpecial()
                 {
@@ -151,15 +151,15 @@ namespace BlazingPizza.Server
                     Name = "Buffalo chicken",
                     Description = "Spicy chicken, hot sauce and bleu cheese, guaranteed to warm you up",
                     BasePrice = 12.75m,
-                    ImageUrl = "/img/pizzas/pizza-1.jpg",
+                    ImageUrl = "img/pizzas/meaty.jpg",
                 },
                 new PizzaSpecial()
                 {
                     Id = 5,
                     Name = "Mushroom Lovers",
-                    Description = "It's has mushrooms. Isn't that obvious?",
+                    Description = "It has mushrooms. Isn't that obvious?",
                     BasePrice = 11.00m,
-                    ImageUrl = "/img/pizzas/pizza-1.jpg",
+                    ImageUrl = "img/pizzas/mushroom.jpg",
                 },
                 new PizzaSpecial()
                 {
@@ -167,15 +167,15 @@ namespace BlazingPizza.Server
                     Name = "The Brit",
                     Description = "When in London...",
                     BasePrice = 10.25m,
-                    ImageUrl = "/img/pizzas/pizza-1.jpg",
+                    ImageUrl = "img/pizzas/brit.jpg",
                 },
                 new PizzaSpecial()
                 {
                     Id = 7,
                     Name = "Veggie Delight",
-                    Description = "It's like salad but on a pizza",
+                    Description = "It's like salad, but on a pizza",
                     BasePrice = 11.50m,
-                    ImageUrl = "/img/pizzas/pizza-1.jpg",
+                    ImageUrl = "img/pizzas/salad.jpg",
                 },
                 new PizzaSpecial()
                 {
@@ -183,7 +183,7 @@ namespace BlazingPizza.Server
                     Name = "Margherita",
                     Description = "Traditional Italian pizza with tomatoes and basil",
                     BasePrice = 9.99m,
-                    ImageUrl = "/img/pizzas/pizza-1.jpg",
+                    ImageUrl = "img/pizzas/margherita.jpg",
                 },
             };
 
