52ce19e4d1ed8a3894f9b0bdb5bd973125e5cb80
Ed Charbeneau <ed.charbeneau@gmail.com>
2019-06-01 19:55:37 -0400

    Prior to the step 3, the PlaceOrder post action is missing the return type and return order.OrderId. Instead the controller is of and NoContent is returned. This breaks the workshop at the end of step 3 instructions.

--- save-points/00-Starting-point/BlazingPizza.Server/OrdersController.cs
+++ save-points/00-Starting-point/BlazingPizza.Server/OrdersController.cs
@@ -51,7 +51,7 @@ namespace BlazingPizza.Server
         }
 
         [HttpPost]
-        public async Task<ActionResult> PlaceOrder(Order order)
+        public async Task<ActionResult<int>> PlaceOrder(Order order)
         {
             order.CreatedTime = DateTime.Now;
             order.DeliveryLocation = new LatLong(51.5001, -0.1239);
@@ -59,7 +59,7 @@ namespace BlazingPizza.Server
 
             _db.Orders.Attach(order);
             await _db.SaveChangesAsync();
-            return NoContent();
+            return order.OrderId;
         }
 
         private string GetUserId()
