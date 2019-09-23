8434a70d9df6108e812e167b303730a4fd2c14a5
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-09-23 12:09:14 +0100

    Update namespace in ServerAuthenticationStateProvider

--- src/BlazingPizza.Client/ServerAuthenticationStateProvider.cs
+++ src/BlazingPizza.Client/ServerAuthenticationStateProvider.cs
@@ -1,7 +1,7 @@
 ﻿using System.Net.Http;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace BlazingPizza.Client
 {
