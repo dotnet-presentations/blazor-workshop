89e5e43906053371d778fc2e0543f8cbeb7e3130
Daniel Roth <daroth@microsoft.com>
2020-04-04 20:09:33 -0700

    Update 06-authentication-and-authorization

---
+++ save-points/06-authentication-and-authorization/BlazingPizza.Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddScoped<OrderState>();
+
+            // Add auth services
+            builder.Services.AddRemoteAuthentication<PizzaAuthenticationState, ApiAuthorizationProviderOptions>();
+            builder.Services.AddApiAuthorization(options =>
+            {
+                options.AuthenticationPaths.LogOutSucceededPath = "";
+                options.ProviderOptions.ConfigurationEndpoint = "_configuration/BlazingPizza.Client"; // temporary workaround
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
