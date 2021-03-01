7ff37c5f12b2cfb0db15faf062c3a7279da35174
jasonn0118 <tlswoals2006@gmail.com>
2021-02-28 16:58:51 -0800

    Implemented Authentication/Authorization

---
+++ save-points/00-get-started/BlazingPizza.Client/PizzaAuthenticationState.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Client
+{
+    public class PizzaAuthenticationState : RemoteAuthenticationState
+    {
+        public Order Order { get; set; }
+    }
+}
