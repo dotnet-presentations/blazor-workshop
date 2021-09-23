06453936d07c8eae52d16a013f223a64cf9e36e9
David Pine <david.pine@microsoft.com>
2021-09-23 10:37:01 -0500

    Added full support of SignalR server & client bits, minor clean up along the way.

--- src/BlazingPizza.ComponentsLibrary/Map/Marker.cs
+++ src/BlazingPizza.ComponentsLibrary/Map/Marker.cs
@@ -1,12 +1,8 @@
 ﻿namespace BlazingPizza.ComponentsLibrary.Map
 {
-    public class Marker
+    public class Marker : Point
     {
         public string Description { get; set; }
-
-        public double X { get; set; }
-
-        public double Y { get; set; }
 
         public bool ShowPopup { get; set; }
     }
---
+++ src/BlazingPizza.Shared/OrderStatusHubConsts.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazingPizza
+{
+    public class OrderStatusHubConsts
+    {
+        public class MethodNames
+        {
+            public const string StartTrackingOrder = nameof(StartTrackingOrder);
+            public const string StopTrackingOrder = nameof(StopTrackingOrder);
+        }
+
+        public class EventNames
+        {
+            public const string OrderStatusChanged = nameof(OrderStatusChanged);
+        }
+    }
+}
