a5464670195968fa169b320a127f872cdd09f8f4
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2022-01-28 17:44:38 -0500

    Finished savepoint 0 and 1

--- save-points/01-Components-and-layout/BlazingPizza.Server/Program.cs
+++ save-points/01-Components-and-layout/BlazingPizza.Server/Program.cs
@@ -1,33 +1,67 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using BlazingPizza.Server;
 
-namespace BlazingPizza.Server
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews()
+	.AddJsonOptions(options =>  {
+		options.JsonSerializerOptions.AddContext<BlazingPizza.OrderContext>();
+	});
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<PizzaStoreContext>(options =>
+		options.UseSqlite("Data Source=pizza.db")
+			.UseModel(BlazingPizza.Server.Models.PizzaStoreContextModel.Instance));
+
+builder.Services.AddDefaultIdentity<PizzaStoreUser>(options => options.SignIn.RequireConfirmedAccount = true)
+		.AddEntityFrameworkStores<PizzaStoreContext>();
+
+builder.Services.AddIdentityServer()
+		.AddApiAuthorization<PizzaStoreUser, PizzaStoreContext>();
+
+builder.Services.AddAuthentication()
+		.AddIdentityServerJwt();
+
+var app = builder.Build();
+
+// Initialize the database
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-
-            // Initialize the database
-            var scopeFactory = host.Services.GetRequiredService<IServiceScopeFactory>();
-            using (var scope = scopeFactory.CreateScope())
-            {
-                var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
-                if (db.Database.EnsureCreated())
-                {
-                    SeedData.Initialize(db);
-                }
-            }
-
-            host.Run();
-        }
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+	var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+	if (db.Database.EnsureCreated())
+	{
+		SeedData.Initialize(db);
+	}
 }
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseIdentityServer();
+app.UseAuthorization();
+
+app.MapPizzaApi();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
