1279c3f221363c2cb3a4face48d3247b3063a883
ed.charbeneau@gmail.com <ed.charbeneau@gmail.com>
2023-09-18 15:47:29 -0400

    Synced dependencies with previous lesson

--- save-points/07-javascript-interop/BlazingPizza.ComponentsLibrary/Map/Marker.cs
+++ save-points/07-javascript-interop/BlazingPizza.ComponentsLibrary/Map/Marker.cs
@@ -2,7 +2,7 @@
 
 public class Marker
 {
-    public string Description { get; set; }
+    public string Description { get; set; } = string.Empty;
 
     public double X { get; set; }
 
---
+++ save-points/07-javascript-interop/BlazingPizza.Server/NotificationsController.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazingPizza.Server
+{
+    [Route("notifications")]
+    [ApiController]
+    [Authorize]
+    public class NotificationsController : Controller
+    {
+        private readonly PizzaStoreContext _db;
+
+        public NotificationsController(PizzaStoreContext db)
+        {
+            _db = db;
+        }
+
+        [HttpPut("subscribe")]
+        public async Task<NotificationSubscription> Subscribe(NotificationSubscription subscription)
+        {
+            // We're storing at most one subscription per user, so delete old ones.
+            // Alternatively, you could let the user register multiple subscriptions from different browsers/devices.
+            var userId = PizzaApiExtensions.GetUserId(HttpContext);
+            if (userId is null)
+            {
+                throw new UnauthorizedAccessException();
+            }
+            var oldSubscriptions = _db.NotificationSubscriptions.Where(e => e.UserId == userId);
+            _db.NotificationSubscriptions.RemoveRange(oldSubscriptions);
+
+            // Store new subscription
+            subscription.UserId = userId;
+            _db.NotificationSubscriptions.Attach(subscription);
+
+            await _db.SaveChangesAsync();
+            return subscription;
+        }
+
+    }
+}
